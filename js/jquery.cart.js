f04ee9453c942a2d6120537945b5953554d200e9
omgjwood <justinemwood@gmail.com>
2017-03-08 11:57:19 -0600

    took off theshop.js and added to the cart.js.  went in and added a local storage if statement

--- js/jquery.cart.js
+++ js/jquery.cart.js
@@ -1,11 +1,43 @@
 $(document).ready(function() {
     
     
-    if(!localStorage.getItem("pbh_cart") {
+    
+    /*this is where we are creating the local storage. at first its saying if the local storage ISNT there then it needs to be created.  Then we are creating the local sotrage by creating a var of pbh_cart.*/ 
+    if(!localStorage.getItem("pbh_cart")) {
        var pbh_cart = {};
        pbh_cart.products = [];
        localStorage.setItem ("pbh_cart", JSON.stringify(pbh_cart));
-       }
+    }
+    
+ 
+    /*this is where we created the function for collecting the product information when clicking the "add to cart button*/              
+    $("#product").submit(function(e) {
+        e.preventDefault();
+        itemPrice = $("#item_price").text();
+        itemName = $("#item_name").text();
+        itemImg = $("#item_main_img").attr('src');
+        itemQty = $("#quantity").val();
+        itemUrl = window.location.href;
+        itemProductID = $("#item_productID").val();
+        
+        product = {"itemPrice": itemPrice,
+                   "itemName": itemName,
+                   "itemImg": itemImg,
+                   "itemQty": itemQty,
+                   "itemUrl": itemUrl,
+                   "itemProductID": itemProductID};
+    
+        
+        console.log(product);    
+        console.log(product.itemPrice);
+        console.log(product.itemImg);
+        console.log(product.itemName);
+        console.log(product.itemQty);
+        console.log(product.itemUrl);
+        console.log(product.itemProductID);
+    });
     
     
-)};
+   
+});
+
