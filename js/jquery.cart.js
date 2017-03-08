85e5b1d3d4596747abb3f73fec716b0a46a386e3
omgjwood <justinemwood@gmail.com>
2017-03-08 13:34:18 -0600

    added to cart function, fixed the number cart icon and added them.

--- js/jquery.cart.js
+++ js/jquery.cart.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
         itemPrice = $("#item_price").text();
         itemName = $("#item_name").text();
         itemImg = $("#item_main_img").attr('src');
-        itemQty = $("#quantity").val();
+        itemQty = Number($("#quantity").val());
         itemUrl = window.location.href;
         itemProductID = $("#item_productID").val();
         
@@ -26,18 +26,47 @@ $(document).ready(function() {
                    "itemQty": itemQty,
                    "itemUrl": itemUrl,
                    "itemProductID": itemProductID};
-    
         
+        addToCart(product);
+        
+        /*
         console.log(product);    
         console.log(product.itemPrice);
         console.log(product.itemImg);
         console.log(product.itemName);
         console.log(product.itemQty);
         console.log(product.itemUrl);
-        console.log(product.itemProductID);
+        console.log(product.itemProductID); */
     });
     
-    
+    function addToCart(product) {
+        //get cart object from local storage, unstring it
+        var pbh_cart = JSON.parse(localStorage.pbh_cart);
+        
+        //check if the item is already in the cart
+        var isItemInCart = false;
+        pbh_cart.products.forEach(function(p) {
+            if(p.itemProductID == product.itemProductID) {
+                p.itemQty += product.itemQty;
+                isItemInCart = true;
+            }
+            
+        });
+        
+        //add product to cart object, 
+        if(!isItemInCart) { pbh_cart.products.push(product)};
+        
+        //update the shopping cart icon
+        var cartQty = Number($("#cart_qty").text());
+        cartQty += product.itemQty;
+        
+        $("#cart_qty").text(cartQty);
+        
+        //return cart data to local storage, restring it
+        localStorage.setItem ("pbh_cart", JSON.stringify(pbh_cart));
+        
+         
+    }
    
 });
 
